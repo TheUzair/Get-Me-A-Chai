228fa422bb070a0a35a710dbcda892b567a31a5e
TheUzair <theuzair_mohujer90@gmail.com>
2024-11-25 09:23:28 +0530

    Fix DB connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,13 +1,12 @@
 "use server"
 import mongoose from 'mongoose';
-
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) {
     console.log("MongoDB already connected.");
     return;
   }
   try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/patreon`);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error: ", error.message);
